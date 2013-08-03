132d33666040ea3601cf4a6e716af92d0bb85ba1
billstron <billstron@gmail.com>
2013-08-03 13:20:54 -0400

    testing with drag and drop

--- dropbox-tree.js
+++ dropbox-tree.js
@@ -127,9 +127,9 @@
 				// PWS status OK
 				node.addChild(data);
 				node.setLazyNodeStatus(DTNodeStatus_Ok);
-				
+
 				var event = "nodeLoaded.dynatree." + node.tree.$tree.attr("id") + "." + node.data.key;
-				node.tree.$tree.trigger(event, [node, true]);
+				node.tree.$tree.trigger(event, [ node, true ]);
 			});
 		}
 
@@ -175,6 +175,70 @@
 					},
 					onDblClick : function(node, event) {
 						$(myself).trigger("doubleClick", node, event);
+					},
+					dnd : {
+						onDragStart : function(node) {
+							/**
+							 * This function MUST be defined to enable dragging
+							 * for the tree. Return false to cancel dragging of
+							 * node.
+							 */
+							console.log("tree.onDragStart(%o)", node);
+							return true;
+						},
+						onDragStop : function(node) {
+							// This function is optional.
+							console.log("tree.onDragStop(%o)", node);
+						},
+						autoExpandMS : 1000,
+						preventVoidMoves : true, // Prevent dropping nodes
+													// 'before self', etc.
+						onDragEnter : function(node, sourceNode) {
+							/**
+							 * sourceNode may be null for non-dynatree
+							 * droppables. Return false to disallow dropping on
+							 * node. In this case onDragOver and onDragLeave are
+							 * not called. Return 'over', 'before, or 'after' to
+							 * force a hitMode. Return ['before', 'after'] to
+							 * restrict available hitModes. Any other return
+							 * value will calc the hitMode from the cursor
+							 * position.
+							 */
+							console.log("tree.onDragEnter(%o, %o)", node, sourceNode);
+							return true;
+						},
+						onDragOver : function(node, sourceNode, hitMode) {
+							/**
+							 * Return false to disallow dropping this node.
+							 * 
+							 */
+//							console.log("tree.onDragOver(%o, %o, %o)", node, sourceNode, hitMode);
+//							// Prevent dropping a parent below it's own child
+//							if (node.isDescendantOf(sourceNode)) {
+//								return false;
+//							}
+//							// Prohibit creating childs in non-folders (only
+//							// sorting allowed)
+//							if (!node.data.isFolder && hitMode === "over") {
+//								return "after";
+//							}
+						},
+						onDrop : function(node, sourceNode, hitMode, ui, draggable) {
+							/**
+							 * This function MUST be defined to enable dropping
+							 * of items on the tree.
+							 */
+							console.log("tree.onDrop(%o, %o, %s)", node, sourceNode, hitMode);
+							sourceNode.move(node, hitMode);
+							// expand the drop target
+							// sourceNode.expand(true);
+						},
+						onDragLeave : function(node, sourceNode) {
+							/**
+							 * Always called if onDragEnter was called.
+							 */
+							console.log("tree.onDragLeave(%o, %o)", node, sourceNode);
+						}
 					}
 				});
 				var rootNode = $("#tree").dynatree("getRoot");
